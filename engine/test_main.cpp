fc19ed86a4d20fb1be4c9af8af55db0e920a34a0
Zor0 <gayahh123456789@gmail.com>
2026-09-22 18:31:42 +1000

    Add C++ cricket physics engine foundation

---
+++ engine/test_main.cpp
@@ -0,0 +1,4 @@
+#include "CricketPhysics.hpp"
+#include <cassert>
+#include <iostream>
+int main(){cricket::CricketPhysics e;cricket::Delivery d;d.speedKph=135;d.length=.55f;e.startDelivery(d);for(int i=0;i<120&&e.ball().active;i++)e.update(1.f/120);e.reset();e.startDelivery(d);cricket::BatterInput in;in.timing=.5f;in.power=.9f;in.shot=cricket::ShotType::Drive;auto c=e.strike(in);assert(c.connected&&c.quality>.5f&&c.exitSpeed>30);std::cout<<"Cricket engine smoke test passed. Quality="<<c.quality<<"\n";}
